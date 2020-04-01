fa64f06af6fc95af2ab57c8500c7e51e62f532a2
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-04-02 02:21:36 +0900

    Add: *EVENT-ATTRIBUTES*

--- markup-functions.asd
+++ markup-functions.asd
@@ -2,7 +2,7 @@
 (in-package :asdf)
 (defsystem "markup-functions"
   :version
-  "0.17.0"
+  "0.18.0"
   :depends-on
   nil
   :pathname
--- src/markup-functions.lisp
+++ src/markup-functions.lisp
@@ -33,6 +33,115 @@
                ; translated or not
     ))
 
+(defparameter *event-attributes*
+  '(
+    ;; Window events.
+    :onafterprint ; script Script to be run after the document is printed
+    :onbeforeprint ; script Script to be run before the document is printed
+    :onbeforeunload ; script Script to be run when the document is about to be
+                    ; unloaded
+    :onerror ; script Script to be run when an error occurs
+    :onhashchange ; script Script to be run when there has been changes to the
+                  ; anchor part of the a URL
+    :onload ; script Fires after the page is finished loading
+    :onmessage ; script Script to be run when the message is triggered
+    :onoffline ; script Script to be run when the browser starts to work offline
+    :ononline ; script Script to be run when the browser starts to work online
+    :onpagehide ; script Script to be run when a user navigates away from a page
+    :onpageshow ; script Script to be run when a user navigates to a page
+    :onpopstate ; script Script to be run when the window's history changes
+    :onresize ; script Fires when the browser window is resized
+    :onstorage ; script Script to be run when a Web Storage area is updated
+    :onunload ; script Fires once a page has unloaded (or the browser window
+              ; has been closed)
+    ;; Form events
+    :onblur ; Fires the moment that the element loses focus
+    :onchange ; Fires the moment when the value of the element is changed
+    :oncontextmenu ; Script to be run when a context menu is triggered
+    :onfocus ; Fires the moment when the element gets focus
+    :oninput ; Script to be run when an element gets user input
+    :oninvalid ; Script to be run when an element is invalid
+    :onreset ; Fires when the Reset button in a form is clicked
+    :onsearch ; Fires when the user writes something in a search field (for
+              ; <input="search">)
+    :onselect ; Fires after some text has been selected in an element
+    :onsubmit ; Fires when a form is submitted
+    ;; Keyboard events
+    :onkeydown ; Fires when a user is pressing a key
+    :onkeypress ; Fires when a user presses a key
+    :onkeyup ; Fires when a user releases a key
+    ;; Mouse events
+    :onclick ; Fires on a mouse click on the element
+    :ondblclick ; Fires on a mouse double-click on the element
+    :onmousedown ; Fires when a mouse button is pressed down on an element
+    :onmousemove ; Fires when the mouse pointer is moving while it is over an
+                 ; element
+    :onmouseout ; Fires when the mouse pointer moves out of an element
+    :onmouseover ; Fires when the mouse pointer moves over an element
+    :onmouseup ; Fires when a mouse button is released over an element
+    :onmousewheel ; Deprecated. Use the onwheel attribute instead
+    :onwheel ; Fires when the mouse wheel rolls up or down over an element
+    ;; Drag events
+    :ondrag ; Script to be run when an element is dragged
+    :ondragend ; Script to be run at the end of a drag operation
+    :ondragenter ; Script to be run when an element has been dragged to a valid
+                 ; drop target
+    :ondragleave ; Script to be run when an element leaves a valid drop target
+    :ondragover ; Script to be run when an element is being dragged over a
+                ; valid drop target
+    :ondragstart ; Script to be run at the start of a drag operation
+    :ondrop ; Script to be run when dragged element is being dropped
+    :onscroll ; Script to be run when an element's scrollbar is being scrolled
+    ;; Clipboard events
+    :oncopy ; Fires when the user copies the content of an element
+    :oncut ; Fires when the user cuts the content of an element
+    :onpaste ; Fires when the user pastes some content in an element
+    ;; Media events
+    :onabort ; Script to be run on abort
+    :oncanplay ; Script to be run when a file is ready to start playing (when
+               ; it has buffered enough to begin)
+    :oncanplaythroug ; Script to be run when a file can be played all the way
+                     ; to the end without pausing for buffering
+    :oncuechange ; Script to be run when the cue changes in a <track> element
+    :ondurationchang ; Script to be run when the length of the media changes
+    :onemptied ; Script to be run when something bad happens and the file is
+               ; suddenly unavailable (like unexpectedly disconnects)
+    :onended ; Script to be run when the media has reach the end (a useful
+             ; event for messages like "thanks for listening")
+    :onerror ; Script to be run when an error occurs when the file is being
+             ; loaded
+    :onloadeddata ; Script to be run when media data is loaded
+    :onloadedmetadat ; Script to be run when meta data (like dimensions and
+                     ; duration) are loaded
+    :onloadstart ; Script to be run just as the file begins to load before
+                 ; anything is actually loaded
+    :onpause ; Script to be run when the media is paused either by the user or
+             ; programmatically
+    :onplay ; Script to be run when the media is ready to start playing
+    :onplaying ; Script to be run when the media actually has started playing
+    :onprogress ; Script to be run when the browser is in the process of
+                ; getting the media data
+    :onratechange ; Script to be run each time the playback rate changes (like
+                  ; when a user switches to a slow motion or fast forward mode)
+    :onseeked ; Script to be run when the seeking attribute is set to false
+              ; indicating that seeking has ended
+    :onseeking ; Script to be run when the seeking attribute is set to true
+               ; indicating that seeking is active
+    :onstalled ; Script to be run when the browser is unable to fetch the media
+               ; data for whatever reason
+    :onsuspend ; Script to be run when fetching the media data is stopped
+               ; before it is completely loaded for whatever reason
+    :ontimeupdate ; Script to be run when the playing position has changed
+                  ; (like when the user fast forwards to a different point in
+                  ; the media)
+    :onvolumechange ; Script to be run each time the volume is changed which
+                    ; (includes setting the volume to "mute")
+    :onwaiting ; Script to be run when the media has paused but is expected to
+               ; resume (like when the media pauses to buffer more data)
+    ;; Misc event
+    :ontoggle ; Fires when the user opens or closes the <details> element
+    ))
+
 (defun pprint-attributes (stream args &rest noise)
   (declare (ignore noise))
   (setf stream (or stream *standard-output*))
