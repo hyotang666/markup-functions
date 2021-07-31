da6a58a4443c9f0f8647f9cd1555cd05a95ceb2d
SATO Shinichi <hyotang.karakoma@gmail.com>
2021-07-31 15:48:38 +0900

    Add: *SVG-PRESENTATION-ATTRIBUTES*.

--- src/svg.lisp
+++ src/svg.lisp
@@ -51,6 +51,24 @@
 (defparameter *svg-document-element-event-attributes*
   (table<-list '(:oncopy :oncut :onpaste)))
 
+(defparameter *svg-presentation-attributes*
+  (table<-list
+    '(:alignment-baseline :baseline-shift :clip :clip-path :clip-rule :color
+      :color-interpolation :color-interpolation-filters :color-profile
+      :color-rendering :cursor :direction :display :dominant-baseline
+      :enable-background :fill :fill-opacity :fill-rule :filter :flood-color
+      :flood-opacity :font-family :font-size :font-size-adjust :font-stretch
+      :font-style :font-variant :font-weight :glyph-orientation-horizontal
+      :glyph-orientation-vertical :image-rendering :kerning :letter-spacing
+      :lighting-color :marker-end :marker-mid :marker-start :mask :opacity
+      :overflow :pointer-events :shape-rendering :solid-color :solid-opacity
+      :stop-color :stop-opacity :stroke :stroke-dasharray :stroke-dashoffset
+      :stroke-linecap :stroke-linejoin :stroke-miterlimit :stroke-opacity
+      :stroke-width :text-anchor :text-decoration :text-rendering :transform
+      :unicode-bidi :vector-effect :visibility :word-spacing :writing-mode))
+  ;; https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/Presentation
+  "SVG presentation attributes are CSS properties that can be used as attributes on SVG elements.")
+
 #++
 ((lambda (string)
    ;; https://svgwg.org/svg2-draft/struct.html#SVGElement
@@ -73,7 +91,7 @@
      (list *svg-aria-attributes* *svg-conditional-processing-attributes*
            *svg-core-attributes* *svg-global-event-attributes*
            *svg-document-element-event-attributes*
-           (table<-list '(:pathlength :d)))))
+           *svg-presentation-attributes* (table<-list '(:pathlength :d)))))
 
 (define-element svg
   (:attributes
